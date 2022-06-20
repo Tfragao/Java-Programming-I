af096063ed660162ac6c59ac5e5d989788b2dafa
Tfragao <fragaot@gmail.com>
2022-06-20 10:57:31 +0200

    Add files via upload

---
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int userInput = Integer.valueOf(scanner.nextLine());
+        int fact = 1;
+        
+        if (userInput == 0){
+            fact = 1;
+        }
+        
+        for (int i = 1; i <= userInput; i++){
+            fact *= i;
+        }
+        System.out.println("Factorial:" + fact);
+    }
+}
---
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class RepeatingBreakingAndRemembering {
+
+    public static void main(String[] args) {
+
+        // This exercise is worth five exercise points, and it is 
+        // gradually extended part by part.
+        // If you want, you can send this exercise to the server
+        // when it's just partially done. In that case the server will complain about 
+        // the parts you haven't done, but you'll get points for the finished parts.
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give numbers:");
+        int sum = 0, totalNumber = 0, even = 0, odd = 0;
+        double average = 0.0;
+        
+        while (true) {
+            int userInput = Integer.valueOf(scanner.nextLine());
+            if (userInput == -1) {
+                System.out.println("Thx! Bye!");
+                break;
+            }
+            sum += userInput;
+            totalNumber++;
+            average = sum /(double)totalNumber;
+            
+            if(userInput % 2 == 0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
+        System.out.println("Sum:" + sum);
+        System.out.println("Numbers: " + totalNumber);
+        System.out.println("Average: " + average);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+    }
+}
